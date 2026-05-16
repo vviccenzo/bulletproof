b78415a0a9cc879b5db5642545003cc563d5f30d
vviccenzo <vvpcraft@gmail.com>
2026-05-16 09:55:08 -0300

    adjust method

--- src/main/java/com/api/bulletproof/repository/WalletRepository.java
+++ src/main/java/com/api/bulletproof/repository/WalletRepository.java
@@ -2,16 +2,19 @@ package com.api.bulletproof.repository;
 
 import com.api.bulletproof.entity.Wallet;
 import jakarta.persistence.LockModeType;
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface WalletRepository extends JpaRepository<Wallet, UUID> {
 
     @Override
     @Lock(value = LockModeType.PESSIMISTIC_READ)
-    Optional<Wallet> findById(UUID id);
+    Optional<Wallet> findById(@NonNull UUID id);
 
 }
